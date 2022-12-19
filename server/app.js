579fe63c14afd4a0944a938c528b47520cfe171b
Arafat Mamia <arafmamia@gmail.com>
2022-12-19 11:16:25 +0300

    setup server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const moviesRouter = require("./routes/movie.router");
 const app = express();
 
+app.use(express.json());
+app.use("/movie", moviesRouter);
 module.exports = app;
